7ca61f3f2a9f95ac97ee20a5604cf7c3d95519b0
Arash Kaffamanesh <ak@cloudssky.com>
2020-07-20 15:35:19 +0200

    helm push image added

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const PORT = 8080;
 
 // create a route for the app
 app.get('/', (req, res) => {
-  res.send('Hello. Breast cancer death rates declined 40% from 1989 to 2016 among women.<p><a href="https://www.nationalbreastcancer.org/early-detection-of-breast-cancer/">The progress is attributed to improvements in early detection.</a>');
+  res.send('Hello Tekton on K3s Bonsai.</a>');
 });
 
 // make the server listen to requests
